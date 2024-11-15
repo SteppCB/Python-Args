31c7e282322a52633e710208adb96c2eac17b865
SteppCB <152199708+SteppCB@users.noreply.github.com>
2024-11-15 13:53:55 -0500

    Update e5_args.py

--- e5_args.py
+++ e5_args.py
@@ -5,30 +5,27 @@ from e4_search_dir import search_file_by_name
 import argparse
 
 def main():
-    # Make an argument parser with the description "File and directory manipulation script"
-    parser = None
-    # Add a positional argument for the directory with the help text "The target directory for the action"
-
-    # Add an argument "--list" with the action "store_true" and the help text "List files and directories"
-
-    # Add an argument "--delete" with the action "store_true" and the help text "Delete a file or directory"
-
-    # Add an argument "--rename" with the help text "Rename a file or directory"
-
-    # Add an argument "--search" with the help text "Search for a file by name"
-
+    # Argument parser
+    parser = argparse.ArgumentParser(description="File and directory manipulation script")
+    parser.add_argument("directory", help="The target directory for the action")
+    parser.add_argument("--list", action="store_true", help="List files and directories")
+    parser.add_argument("--delete", help="Delete a file or directory")
+    parser.add_argument("--rename", nargs=2, metavar=("OLD_NAME", "NEW_NAME"), help="Rename a file or directory")
+    parser.add_argument("--search", help="Search for a file by name")
 
     # Parse the arguments
-    args = None
+    args = parser.parse_args()
 
-    if is_list:
-        list_files_and_directories()
-    elif is_delete:
-        delete_file_or_directory()
-    elif is_rename:
-        rename_file_or_directory()
-    elif is_search:
-        search_file_by_name()
+    if args.list:
+        list_files_and_directories(args.directory)
+    elif args.delete:
+        delete_file_or_directory(args.delete)
+    elif args.rename:
+        rename_file_or_directory(args.rename[0], args.rename[1])
+    elif args.search:
+        search_file_by_name(args.directory, args.search)
+    else:
+        print("No valid action specified. Use --help for more information.")
 
 if __name__ == "__main__":
     main()
